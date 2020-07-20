829e157c935284c9b61161e714d655e7673c7221
Rafay Ghafoor <rafayghafoor@pm.me>
2020-07-20 19:15:21 +0500

    feat(design): Added notification for info and changed placeholder

--- src/components/selector.js
+++ src/components/selector.js
@@ -1,7 +1,17 @@
 import makeAnimated from "react-select/animated";
 import React, { Component } from "react";
 import Select from "react-select";
+import { notification } from "antd";
 import { generateListOfCourses } from "../api/fetch";
+
+const openNotification = () => {
+  notification.info({
+    message: "Display Courses",
+    duration: 3.5,
+    description:
+      "In order to display courses information, close the menu after selecting courses.",
+  });
+};
 
 const filterCourses = () => {
   const courses = generateListOfCourses().then((data) => {
@@ -20,6 +30,7 @@ class CourseSelection extends Component {
     super(props);
     this.state = {
       data: [],
+      showNotification: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -40,21 +51,29 @@ class CourseSelection extends Component {
     const animatedComponents = makeAnimated();
 
     return (
-      <Select
-        closeMenuOnSelect={false}
-        components={animatedComponents}
-        isMulti
-        options={this.state.data}
-        onChange={this.handleChange}
-        onMenuClose={this.props.loadCoursesInfo}
-        theme={(theme) => ({
-          ...theme,
-          borderRadius: 30,
-          colors: {
-            ...theme.colors,
-          },
-        })}
-      />
+      <div>
+        <Select
+          closeMenuOnSelect={false}
+          components={animatedComponents}
+          isMulti
+          options={this.state.data}
+          onChange={this.handleChange}
+          onMenuClose={this.props.loadCoursesInfo}
+          onFocus={() => {
+            openNotification();
+          }}
+          placeholder="Search Courses.... | Close Menu to Load Courses"
+          theme={(theme) => ({
+            ...theme,
+            borderRadius: 30,
+            height: 50,
+            width: "20px",
+            colors: {
+              ...theme.colors,
+            },
+          })}
+        />
+      </div>
     );
   }
 }
